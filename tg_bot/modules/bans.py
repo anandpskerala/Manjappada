51896a8d9d8a8cdd3b8059656405d0bab0ebdc03
siyadkmzia <40415560+siyadkmzia@users.noreply.github.com>
2019-08-19 14:07:00 +0530

    Update bans.py

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -61,7 +61,7 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
     try:
         chat.kick_member(user_id)
         bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
-        message.reply_text("{} Banned!")
+        message.reply_text("{} Banned!".format(mention_html(user.id, user.first_name))
         return log
 
     except BadRequest as excp:
